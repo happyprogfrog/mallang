e5a284a1439d432233d57fac8035c17e5eebd318
happyprogfrog <happyprogfrog@gmail.com>
2024-06-28 10:45:22 +0900

    프로젝트 생성

---
+++ src/main/java/me/progfrog/mallang/MallangApplication.java
@@ -0,0 +1,13 @@
+package me.progfrog.mallang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallangApplication.class, args);
+	}
+
+}
