ec863052e60a687103d2a93214bfaae530be96f9
happyprogfrog <happyprogfrog@gmail.com>
2024-06-28 14:59:34 +0900

    test: 테스트 코드가 통과하도록 RandomGeneratorServiceImpl 로직 구현

--- src/main/java/me/progfrog/mallang/service/RandomGeneratorServiceImpl.java
+++ src/main/java/me/progfrog/mallang/service/RandomGeneratorServiceImpl.java
@@ -2,11 +2,16 @@ package me.progfrog.mallang.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
 public class RandomGeneratorServiceImpl implements RandomGeneratorService {
 
+    final static int MINIMUM_FACTOR = 11;
+    final static int MAXIMUM_FACTOR = 99;
+
     @Override
     public int generateRandomFactor() {
-        return 0;
+        return new Random().nextInt((MAXIMUM_FACTOR - MINIMUM_FACTOR) + 1) + MINIMUM_FACTOR;
     }
 }
