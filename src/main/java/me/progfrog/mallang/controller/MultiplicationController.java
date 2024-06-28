1d775e9b42f7ca343271601d203b2f919fd78926
happyprogfrog <happyprogfrog@gmail.com>
2024-06-28 17:17:24 +0900

    feat: MultiplicationController 구현

--- src/main/java/me/progfrog/mallang/controller/MultiplicationController.java
+++ src/main/java/me/progfrog/mallang/controller/MultiplicationController.java
@@ -1,12 +1,21 @@
 package me.progfrog.mallang.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.progfrog.mallang.domain.Multiplication;
 import me.progfrog.mallang.service.MultiplicationService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-public class MultiplicationController {
+@RequestMapping("/multiplications")
+final class MultiplicationController {
 
     private final MultiplicationService multiplicationService;
+
+    @GetMapping("/random")
+    public Multiplication getRandomMultiplication() {
+        return multiplicationService.createRandomMultiplication();
+    }
 }
