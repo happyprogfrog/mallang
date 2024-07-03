e327f7e8a9f7e28aa794634cffbf2ab0f18da229
happyprogfrog <happyprogfrog@gmail.com>
2024-07-03 09:37:38 +0900

    feat: 리포지토리 추가

---
+++ gamification/src/main/java/me/progfrog/mallang/gamification/repository/ScoreCardRepository.java
@@ -0,0 +1,38 @@
+package me.progfrog.mallang.gamification.repository;
+
+import me.progfrog.mallang.gamification.domain.LeaderBoardRow;
+import me.progfrog.mallang.gamification.domain.ScoreCard;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ScoreCardRepository extends CrudRepository<ScoreCard, Long> {
+
+    /**
+     * ScoreCard의 점수를 합해서 사용자의 총 점수를 조회
+     * @param userId 총 점수를 조회하고자 하는 사용자의 ID
+     * @return 사용자의 총 점수
+     */
+    @Query("SELECT SUM(s.score) "
+            + "FROM me.progfrog.mallang.gamification.domain.ScoreCard s "
+            + "WHERE s.userId = :userId GROUP BY s.userId")
+    int getTotalScoreForUser(@Param("userId") final Long userId);
+
+    /**
+     * 사용자와 사용자의 총 점수를 나타내는 {@link LeaderBoardRow} 리스트를 조회
+     * @return 높은 점수 순으로 정렬된 리터보드
+     */
+    @Query("SELECT NEW me.progfrog.mallang.gamification.domain.LeaderBoardRow(s.userId, SUM(s.score)) "
+            + "FROM me.progfrog.mallang.gamification.domain.ScoreCard s "
+            + "GROUP BY s.userId ORDER BY SUM(s.score) DESC")
+    List<LeaderBoardRow> findFirst10();
+
+    /**
+     * 사용자의 모든 ScoreCard를 조회
+     * @param userId 사용자 ID
+     * @return 특정 사용자의 최근순으로 정렬된 ScoreCard 리스트
+     */
+    List<ScoreCard> findByUserIdOrderByScoreTimestampDesc(final Long userId);
+}
