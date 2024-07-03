3e066dc9c1702b2350ff15bc6f2a502686e6b1b5
happyprogfrog <happyprogfrog@gmail.com>
2024-07-03 09:29:30 +0900

    chore: gamification 모듈 추가

---
+++ gamification/src/test/java/me/progfrog/mallang/gamification/GamificationApplicationTests.java
@@ -0,0 +1,13 @@
+package me.progfrog.mallang.gamification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GamificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
